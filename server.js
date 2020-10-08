b567ec5cca8656104a27de9bd2125dd8b418b718
Scot Woodland <scotwoodland@hotmail.com>
2020-10-08 16:43:04 -0600

    Found an error in server.js - fixed.

--- server.js
+++ server.js
@@ -2,17 +2,19 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { notStrictEqual } = require("assert");
 
 // Preparing variables to invoke express, name the preferred port for Heroku, and create the main directory path 
 const app = express();
 const port = process.env.PORT || 3000;
 const mainDir = path.join(__dirname, "/public");
 
-
+// Setting up express and JSON 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Setting up the notes.html page
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(mainDir, "notes.html"));
 });
@@ -26,10 +28,12 @@ app.get("/api/notes/:id", function(req, res) {
     res.json(savedNotes[Number(req.params.id)]);
 });
 
+// Setting up the index.html page
 app.get("*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
 
+// Using the 'post' command 
 app.post("/api/notes", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
@@ -42,6 +46,7 @@ app.post("/api/notes", function(req, res) {
     res.json(savedNotes);
 })
 
+// Setting up the delete function on the page
 app.delete("/api/notes/:id", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteID = req.params.id;
@@ -60,6 +65,7 @@ app.delete("/api/notes/:id", function(req, res) {
     res.json(savedNotes);
 })
 
+// Setting up the listener function 
 app.listen(port, function() {
     console.log(`Now listening to port ${port}. Thanks for your time and enjoy this quick note taker!`);
 })
